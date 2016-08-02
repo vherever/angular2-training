7fb18fe4e54e7b4dfd400ad62740c74b05992926
vherever <vherever@gmail.com>
2016-08-02 16:24:43 +0300

    input-output relations between parent and child components, alias in inputs

--- input-output_component_relations/app/components/child.component.ts
+++ input-output_component_relations/app/components/child.component.ts
@@ -1,15 +1,14 @@
-import { Component, EventEmitter } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component ({
     selector: 'child',
     templateUrl: 'app/templates/child.component.html',
-    inputs: ['parentValue'],
-    outputs: ['childChanged']
+    inputs: ['parentValue:passedValue'],
 })
 
 export class ChildComponent {
     parentValue: string;
-    childChanged = new EventEmitter<string>();
+    @Output() childChanged = new EventEmitter<string>();
 
     onChange(value: string) {
         this.childChanged.emit(value);
