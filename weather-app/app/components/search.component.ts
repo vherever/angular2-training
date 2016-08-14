2853f3e48b859011635a3c3ddb3bc61177a6a62d
vherever <vherever@gmail.com>
2016-08-14 16:46:39 +0300

    Observable (City found suggestion)

--- weather-app/app/components/search.component.ts
+++ weather-app/app/components/search.component.ts
@@ -1,22 +1,35 @@
-import {Component} from "@angular/core";
-import {ControlGroup} from "@angular/common";
+import {Component, OnInit} from "@angular/core";
 import {WeatherService} from "../services/weather.service";
 import {WeatherItem} from "../models/weather-item.model";
+import {Subject} from "rxjs/Rx";
 @Component({
     selector: 'weather-search',
     templateUrl: 'app/templates/search.component.html'
 })
 
-export class WeatherSearchComponent {
+export class WeatherSearchComponent implements OnInit{
+    private searchStream = new Subject<string>();
+    data: any = {};
+
     constructor(private _weatherService: WeatherService) {}
 
-    onSubmit(form: ControlGroup) {
-        this._weatherService.searchWeatherData(form.value.location)
+    onSubmit() {
+        const weatherItem = new WeatherItem(this.data.name, this.data.weather[0].description, this.data.main.temp);
+        this._weatherService.addWeatherItem(weatherItem);
+    }
+
+    onSearchLocation(cityName: string) {
+        this.searchStream
+            .next(cityName);
+    }
+
+    ngOnInit() {
+        this.searchStream
+            .debounceTime(300)
+            .distinctUntilChanged()
+            .switchMap((input:string) => this._weatherService.searchWeatherData(input))
             .subscribe(
-                data => {
-                    const weatherItem = new WeatherItem(data.name, data.weather[0].description, data.main.temp);
-                    this._weatherService.addWeatherItem(weatherItem);
-                }
+                data => this.data = data
             );
     }
 }
