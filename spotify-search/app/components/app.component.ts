892b1e849016db926e156ba1ae4a6f7593a25c78
vherever <vherever@gmail.com>
2016-08-16 22:52:04 +0300

    ajax call to spotify api

--- spotify-search/app/components/app.component.ts
+++ spotify-search/app/components/app.component.ts
@@ -3,12 +3,14 @@ import {ROUTER_DIRECTIVES} from "@angular/router";
 import {NavbarComponent} from "./navbar.component";
 import {SearchComponent} from "./search.component";
 import {AboutComponent} from "./about.component";
+import {HTTP_PROVIDERS} from '@angular/http';
 
 @Component({
     selector: 'my-app',
     templateUrl: 'app/templates/app.component.html',
     directives: [ROUTER_DIRECTIVES, NavbarComponent],
-    precompile: [SearchComponent, AboutComponent]
+    precompile: [SearchComponent, AboutComponent],
+    providers: [HTTP_PROVIDERS]
 })
 export class AppComponent {
 
--- spotify-search/app/components/search.component.ts
+++ spotify-search/app/components/search.component.ts
@@ -1,8 +1,22 @@
 import {Component} from "@angular/core";
+import {SpotifyService} from "../services/spotify.service";
+
 @Component({
     selector: 'search',
-    templateUrl: 'app/templates/search.component.html'
+    templateUrl: 'app/templates/search.component.html',
+    providers: [SpotifyService]
 })
 export class SearchComponent {
+    searchStr: string;
 
+    constructor(private _spotifyService: SpotifyService) {
+
+    }
+
+    searchMusic() {
+        this._spotifyService.searchMusic(this.searchStr)
+            .subscribe(res => {
+                console.log(res.artists.items);
+            })
+    }
 }
