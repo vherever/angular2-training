9a9f281351114c9803a6da54fb26ad86ac599f4d
vherever <vherever@gmail.com>
2016-08-22 18:18:56 +0300

    basic structure

---
+++ angularclass/src/app/ui/appbar.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-bar',
+    styleUrls: ['app/ui/appbar.css'],
+    templateUrl: 'app/ui/appbar.html'
+})
+export class AppBar {}
